2a7957404c6af4955b751b4806ee87f2cc14ab03
mieubrisse <mieubrisse@gmail.com>
2022-05-03 01:15:46 -0300

    Patching up Grafana launcher

--- kurtosis-module/impl/service_launch_utils/service_launch_utils.go
+++ kurtosis-module/impl/service_launch_utils/service_launch_utils.go
@@ -3,7 +3,6 @@ package service_launch_utils
 import (
 	"github.com/kurtosis-tech/kurtosis-core-api-lib/api/golang/lib/services"
 	"github.com/kurtosis-tech/stacktrace"
-	"io"
 	"os"
 	"text/template"
 )
@@ -28,29 +27,6 @@ func FillTemplateToSharedPath(tmpl *template.Template, data interface{}, destina
 	}
 	if err := tmpl.Execute(destFp, data); err != nil {
 		return stacktrace.Propagate(err, "An error occurred filling the template to destination '%v'", destFilepath)
-	}
-	return nil
-}
-
-func CopyFileToSharedPath(srcFilepathOnModuleContainer string, destSharedPath *services.SharedPath) error {
-	srcFp, err := os.Open(srcFilepathOnModuleContainer)
-	if err != nil {
-		return stacktrace.Propagate(err, "An error occurred opening source file '%v'", srcFilepathOnModuleContainer)
-	}
-
-	destFilepathOnModuleContainer := destSharedPath.GetAbsPathOnThisContainer()
-	destFp, err := os.Create(destFilepathOnModuleContainer)
-	if err != nil {
-		return stacktrace.Propagate(err, "An error occurred creating destination file '%v'", destFilepathOnModuleContainer)
-	}
-
-	if _, err := io.Copy(destFp, srcFp); err != nil {
-		return stacktrace.Propagate(
-			err,
-			"An error occurred copying bytes from source file '%v' to destination file '%v'",
-			srcFilepathOnModuleContainer,
-			destFilepathOnModuleContainer,
-		)
 	}
 	return nil
 }
