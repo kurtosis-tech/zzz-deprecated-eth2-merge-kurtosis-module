361240730d49089c5cb4f3241de265bc9f1cdb7c
Gyanendra Mishra <anomaly.the@gmail.com>
2022-09-08 20:30:02 +0530

    added comment explaining marshaling

--- kurtosis-module/impl/participant_network/cl/cl_node_metrics_info.go
+++ kurtosis-module/impl/participant_network/cl/cl_node_metrics_info.go
@@ -12,6 +12,10 @@ func NewCLNodeMetricsInfo(name string, path string, url string) *CLNodeMetricsIn
 	return &CLNodeMetricsInfo{name: name, path: path, url: url}
 }
 
+// The struct has unexported fields as they start with small letters.
+// These fields don't get Marshaled by default as they are hidden.
+// We define this custom MarshalJSON function so that we can convert this struct to JSON properly.
+// We don't use a pointer receiver as someone might marshal a pointer or a value itself.
 func (cLNodeMetricsInfo CLNodeMetricsInfo) MarshalJSON() ([]byte, error) {
 	return json.Marshal(struct {
 		Name string `json:"name"`
