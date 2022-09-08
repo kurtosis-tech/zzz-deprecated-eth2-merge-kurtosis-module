75075c9f659b804f03033880876e5af793c860aa
Gyanendra Mishra <anomaly.the@gmail.com>
2022-09-08 14:09:18 +0530

    commit files

--- kurtosis-module/impl/grafana/grafana_launcher.go
+++ kurtosis-module/impl/grafana/grafana_launcher.go
@@ -5,8 +5,7 @@ import (
 	"github.com/kurtosis-tech/kurtosis-core-api-lib/api/golang/lib/enclaves"
 	"github.com/kurtosis-tech/kurtosis-core-api-lib/api/golang/lib/services"
 	"github.com/kurtosis-tech/stacktrace"
-	"io"
-	"os"
+	"io/ioutil"
 	"path"
 )
 
@@ -17,14 +16,10 @@ const (
 	httpPortId            = "http"
 	httpPortNumber uint16 = 3000
 
-	configDirectoriesPermission = 0755
+	datasourceConfigRelFilepath = "datasources/datasource.yml"
 
-	datasourcesConfigDirname = "datasources"
-	datasourceConfigFilename = "datasource.yml"
-
-	dashboardsConfigDirname          = "dashboards"
-	dashboardProvidersConfigFilename = "dashboard-providers.yml"
-	dashboardConfigFilename          = "dashboard.json"
+	dashboardProvidersConfigRelFilepath = "dashboards/dashboard-providers.yml"
+	dashboardConfigRelFilepath          = "dashboards/dashboard.json"
 
 	configDirpathEnvVar = "GF_PATHS_PROVISIONING"
 
@@ -81,28 +76,10 @@ func getGrafanaConfigDirArtifactUuid(
 	dashboardProvidersConfigTemplate string,
 	prometheusPrivateUrl string,
 ) (services.FilesArtifactUUID, error) {
-	datasourcesConfigDirpathOnModule := path.Join(grafanaConfigDirpathOnModule, datasourcesConfigDirname)            // /tmp/grafana-config/datasources
-	datasourceConfigFilepath := path.Join(datasourcesConfigDirpathOnModule, datasourceConfigFilename)                // /tmp/grafana-config/datasources/datasources.yml
-	dashboardsConfigDirpathOnModule := path.Join(grafanaConfigDirpathOnModule, dashboardsConfigDirname)              // /tmp/grafana-config/dashboards/
-	dashboardProvidersConfigFilepath := path.Join(dashboardsConfigDirpathOnModule, dashboardProvidersConfigFilename) // /tmp/grafana-config/dashboards/dashboards-provider.yml
-	dashboardConfigFilepath := path.Join(dashboardsConfigDirpathOnModule, dashboardConfigFilename)                   // /tmp/grafana-config/dashboards/dashboards.json
-
 	dashboardConfigFilepathOnGrafanaContainer := path.Join(
 		grafanaConfigDirpathOnService,
-		dashboardsConfigDirname,
-		dashboardConfigFilename,
+		dashboardConfigRelFilepath,
 	) // /config/dashboards/dashboards.json
-
-	dirpathsToCreate := []string{
-		grafanaConfigDirpathOnModule,     // /tmp/grafana-config
-		datasourcesConfigDirpathOnModule, // /tmp/grafana-config/datasources
-		dashboardsConfigDirpathOnModule,  // /tmp/grafana-config/dashboards/
-	}
-	for _, dirpath := range dirpathsToCreate {
-		if err := os.Mkdir(dirpath, configDirectoriesPermission); err != nil {
-			return "", stacktrace.Propagate(err, "An error occurred creating Grafana config directory '%v'", dirpathsToCreate)
-		}
-	}
 
 	datasourceData := datasourceConfigTemplateData{
 		PrometheusURL: prometheusPrivateUrl,
@@ -116,27 +93,18 @@ func getGrafanaConfigDirArtifactUuid(
 	}
 	dashboardProvidersTemplateAndData := enclaves.NewTemplateAndData(dashboardProvidersConfigTemplate, dashboardProvidersData)
 
-	templateAndDataByDestRelFilepath := make(map[string]*enclaves.TemplateAndData)
-	templateAndDataByDestRelFilepath[datasourceConfigFilepath] = datasourceTemplateAndData                 // /tmp/grafana-config/datasources/datasources.yml -> data
-	templateAndDataByDestRelFilepath[dashboardProvidersConfigFilepath] = dashboardProvidersTemplateAndData // /tmp/grafana-config/dashboards/dashboards-provider.yml -> data
-
-	// copies the dashboard.json from /static-files/ to /tmp/grafana-config/dashboards/dashboards.json
-	if err := addGrafanaDashboardConfigToConfigDir(
-		static_files.GrafanaDashboardConfigFilepath,
-		dashboardConfigFilepath,
-	); err != nil {
-		return "", stacktrace.Propagate(
-			err,
-			"An error occurred copying Grafana dashboard config file '%v' to the Grafana config directory at '%v'",
-			static_files.GrafanaDashboardConfigFilepath,
-			dashboardConfigFilepath,
-		)
+	// Even though this is a pure json we treat it as a template
+	dashboardConfigFileContent, err := ioutil.ReadFile(static_files.GrafanaDashboardConfigFilepath)
+	if err != nil {
+		return "", stacktrace.Propagate(err, "An error occurred reading the dashboard file content")
 	}
+	dashboardConfigFileTemplateAndData := enclaves.NewTemplateAndData(string(dashboardConfigFileContent), "")
 
-	// we used to upload /tmp/grafana-config
-	// with the render templates call below we are missing the right structure
-	// we are also missing the dashboard.json
-	// i still need a call to upload files or i can use an empty template
+	templateAndDataByDestRelFilepath := make(map[string]*enclaves.TemplateAndData)
+	templateAndDataByDestRelFilepath[datasourceConfigRelFilepath] = datasourceTemplateAndData                 // /tmp/grafana-config/datasources/datasources.yml -> data
+	templateAndDataByDestRelFilepath[dashboardProvidersConfigRelFilepath] = dashboardProvidersTemplateAndData // /tmp/grafana-config/dashboards/dashboards-provider.yml -> data
+	templateAndDataByDestRelFilepath[dashboardConfigRelFilepath] = dashboardConfigFileTemplateAndData
+
 	artifactUuid, err := enclaveCtx.RenderTemplates(templateAndDataByDestRelFilepath)
 	if err != nil {
 		return "", stacktrace.Propagate(err, "An error occurred rendering Grafana templates")
@@ -163,29 +131,4 @@ func getContainerConfigSupplier(
 	}
 
 	return containerConfigSupplier
-}
-
-func addGrafanaDashboardConfigToConfigDir(srcFilepath, destFilepath string) error {
-	// Copy the config file from the static files
-	srcFp, err := os.Open(srcFilepath)
-	if err != nil {
-		return stacktrace.Propagate(err, "An error occurred opening Grafana dashboard config file '%v'", srcFilepath)
-	}
-	defer srcFp.Close()
-
-	destFp, err := os.Create(destFilepath)
-	if err != nil {
-		return stacktrace.Propagate(err, "An error occurred creating dashboard config file '%v'", destFilepath)
-	}
-	defer destFp.Close()
-
-	if _, err := io.Copy(destFp, srcFp); err != nil {
-		return stacktrace.Propagate(
-			err,
-			"An error occurred copying bytes from dashboard config source file '%v' to destination file '%v'",
-			srcFilepath,
-			destFilepath,
-		)
-	}
-	return nil
 }
